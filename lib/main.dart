231a2a30989729b9cd7bfe38ca5458cc7d23e221
afifudinn <work.afifudin@gmail.com>
2021-11-23 18:59:09 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDark = false;
+
+  Widget form(String placeholder) {
+    return Container(
+      height: 54,
+      width: double.infinity,
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          color: isDark ? Colors.grey.shade900 : Colors.grey.shade200,
+          border: Border.all(color: Colors.blue, width: 2),
+          borderRadius: BorderRadius.circular(12)),
+      child: Align(
+        alignment: Alignment.center,
+        child: TextField(
+            style: GoogleFonts.inter(
+                fontWeight: FontWeight.w500,
+                fontSize: 14,
+                color: isDark ? Colors.grey.shade200 : Colors.grey.shade800),
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                isCollapsed: true,
+                isDense: true,
+                hintText: placeholder,
+                hintStyle: GoogleFonts.inter(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                    color: isDark ? Colors.grey.shade600 : Colors.grey.shade400),
+                border: InputBorder.none)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  PreferredSizeWidget appBar() {
+    return AppBar(
+      backgroundColor: isDark ? Color(0xff101010) : Colors.grey.shade50,
+      elevation: 0,
+      leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: isDark ? Colors.grey.shade50 : Color(0xff101010),
+          )),
+      actions: [
+        IconButton(
+            onPressed: () {
+              setState(() {
+                isDark = !isDark;
+              });
+            },
+            icon: Icon(isDark ? Icons.light_mode : Icons.dark_mode,
+                color: isDark ? Colors.grey.shade50 : Color(0xff101010)))
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: isDark ? Color(0xff101010) : Colors.grey.shade50,
+        appBar: appBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Welcome Back!',
+                        style: GoogleFonts.inter(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: isDark ? Colors.white : Color(0xff101010))),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text('Sign in to your account',
+                        style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500, color: Colors.grey)),
+                    SizedBox(
+                      height: 48,
+                    ),
+                    Text('Email',
+                        style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500,
+                            color: isDark ? Colors.grey.shade200 : Colors.grey.shade800)),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    form('myemail@example.com'),
+                    SizedBox(
+                      height: 36,
+                    ),
+                    Text('Password',
+                        style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500,
+                            color: isDark ? Colors.grey.shade200 : Colors.grey.shade800)),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    form('Your password'),
+                    SizedBox(
+                      height: 36,
+                    ),
+                    Container(
+                      height: 54,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [Colors.blue, Colors.lightBlueAccent]),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Center(
+                        child: Text('Login',
+                            style: GoogleFonts.inter(
+                                fontWeight: FontWeight.w700, color: Colors.grey.shade200)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Dont have an account?',
+                            style: GoogleFonts.inter(
+                                color: isDark ? Colors.grey.shade600 : Colors.grey.shade400,
+                                fontWeight: FontWeight.w500)),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text('Sign Up',
+                            style: GoogleFonts.inter(
+                                color: Colors.blue, fontWeight: FontWeight.w500))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
